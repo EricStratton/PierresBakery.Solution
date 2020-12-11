5de1958d3114d459508fba8dc46975c80b31cb75
Eric Stratton <strattonericj@gmail.com>
2020-12-11 13:05:34 -0800

    continue building out Program file

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -15,15 +15,16 @@ namespace BakeryOrder
       {
         Console.WriteLine("Great, would you like to order pastries or bread? (P/B)");
         string pOrB = Console.ReadLine();
-        if (pOrB == "p" || pOrB = "P")
+        if (pOrB == "p" || pOrB == "P")
         {
           
           Console.WriteLine("Our pastries are some of the best in town!");
-          Console.WriteLine("Pastries cost 2$ per unit, or 5$ for 3, deal can be applied to any amount!");
+          Console.WriteLine("Pastries cost 2$ each, or 5$ for 3, deal can be applied to any amount!");
           Console.WriteLine("How many pastries would you like?");
           string strPastryOrder = Console.ReadLine();
           int pastryOrder = int.Parse(strPastryOrder);
           PastryOrder newPastryOrder = new PastryOrder(pastryOrder);
+          Console.WriteLine(newPastryOrder.Pastries);
         }
       }
     }
