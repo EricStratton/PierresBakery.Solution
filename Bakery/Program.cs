d9b3b7e76265f3aec29bcdbffb1e95108f0c46cb
Eric Stratton <strattonericj@gmail.com>
2020-12-11 14:00:30 -0800

    add bread order logic to Program file

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -38,7 +38,7 @@ namespace BakeryOrder
     public static void PastryOrder()
     {
       Console.WriteLine("Our pastries are some of the best in town!");
-      Console.WriteLine("Pastries cost 2$ each, or 5$ for 3, deal can be applied to any amount!");
+      Console.WriteLine("Pastries cost 2$ each, or 5$ for 3, deal can be applied to any amount.");
       Console.WriteLine("How many pastries would you like?");
       string strPastryOrder = Console.ReadLine();
       int pastryOrder = int.Parse(strPastryOrder);
@@ -49,19 +49,48 @@ namespace BakeryOrder
       {
         Console.WriteLine("Okay your total cost for those pastries will be: ");
         int pastryOrderTotal = newPastryOrder.GetCost(newPastryOrder.Pastries);
-        Console.WriteLine($"${pastryOrderTotal}");
+        Console.WriteLine($"${pastryOrderTotal}\n");
         Main();
       }
       else if (correctPastries == "n" || correctPastries == "N")
       {
-        Console.WriteLine("Okay, lets try again.");
-        Main();
+        Console.WriteLine("Okay, lets try again.\n");
+        PastryOrder();
+      }
+      else
+      {
+        Console.WriteLine("Oops, something went wrong. Let's try again.\n");
+        PastryOrder();
       }
     }
 
     public static void BreadOrder()
     {
-
+      Console.WriteLine("Okay, grab some of our world famous bread!");
+      Console.WriteLine("Loaves of bread cost $5 each, but if you buy 2 we'll throw in a third free, applicable on any order size.");
+      Console.WriteLine("How many loaves of bread would you like?");
+      string strBreadOrder = Console.ReadLine();
+      int breadOrder = int.Parse(strBreadOrder);
+      BreadOrder newBreadOrder = new BreadOrder(breadOrder);
+      Console.WriteLine($"You've ordered {newBreadOrder.Loaves} loaves of bread, is this correct? (Y/N)");
+      string correctLoaves = Console.ReadLine();
+      if (correctLoaves == "y" || correctLoaves == "Y")
+      {
+        Console.WriteLine("Okay your total cost for those loaves of bread comes to: ");
+        int loavesOrderTotal = newBreadOrder.GetCost(newBreadOrder.Loaves);
+        Console.WriteLine($"${loavesOrderTotal}\n");
+        Main();
+      }
+      else if (correctLoaves == "n" || correctLoaves == "N")
+      {
+        Console.WriteLine("Okay, lets try again.\n");
+        BreadOrder();
+      }
+      else
+      {
+        Console.WriteLine("Oops, something went wrong. Let's try again.\n");
+        BreadOrder();
+      }
     }
   }
 }
